008cfc222eb4d03ac5d268a8e1843d224397ad24
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-10-09 20:58:57 -0600

    :lipstick:Changes in structure of FirstModuleScreen

--- src/pages/FirstModuleScreen.jsx
+++ src/pages/FirstModuleScreen.jsx
@@ -1,27 +1,31 @@
 import PropTypes from "prop-types";
 import { useLocation } from "wouter";
 import NavBarComponent from "../components/NavbarComponent";
+import "../styles/firstModuleStyle.css";
 
 const FirstModuleScreen = ({ onSignOut }) => {
-
     const [, setLocation] = useLocation();
 
     const handleLogout = () => {
         onSignOut();
         setLocation("/login");
-    }
+    };
 
     return (
-        <div>
-            <NavBarComponent onSignOut={handleLogout} />
-            <h1>Hola Test</h1>
+        <div className="firstModuleScreenContainer">
+            <div className="navContainer">
+                <NavBarComponent onSignOut={handleLogout} />
+                <div className="formControl">
+                    <section className="sectionForm">
+                    </section>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 FirstModuleScreen.propTypes = {
     onSignOut: PropTypes.func.isRequired,
 };
-
 
 export default FirstModuleScreen;
