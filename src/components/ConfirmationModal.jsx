8f9fdf940086b30767f6e34bdd766df977664e66
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-10-21 08:52:42 -0600

    :rocket:Add confirmation modal component

---
+++ src/components/ConfirmationModal.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import Modal from 'react-modal';
+import "../styles/errorModalStyle.css";
+
+Modal.setAppElement('#root');
+
+function ConfirmationModal({ isOpen, onRequestClose, onConfirm, message, title }) {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="Confirmation Modal"
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                },
+                content: {
+                    border: 'none',
+                    background: 'none'
+                }
+            }}
+        >
+            <div className="confirmation-modal">
+                <h2>{title}</h2>
+                <p>{message}</p>
+                <div className="button-container">
+                    <button className="cancel-button" onClick={onRequestClose}>Cancelar</button>
+                    <button className="create-button" onClick={onConfirm}>Crear</button>
+                </div>
+            </div>
+        </Modal>
+    );
+}
+
+ConfirmationModal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onRequestClose: PropTypes.func.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+    message: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+};
+
+export default ConfirmationModal;
