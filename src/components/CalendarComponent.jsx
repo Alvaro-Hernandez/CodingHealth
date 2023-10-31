23688c97b5dfc5b387d73e80c4f62fb30725e597
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-10-30 23:19:34 -0600

    :sparkles:Change calendar event entries

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,18 +1,41 @@
 import PropTypes from 'prop-types';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment'
+import moment from 'moment';
+import { useState } from 'react';
 
 const localizer = momentLocalizer(moment);
 
-const CalendarComponent = ({ events = [] }) => {
+const CalendarComponent = ({ events = [], onEventClick }) => {
+    const [selecting, setSelecting] = useState(false);
+
+    const handleSelectSlot = ({ start, end }) => {
+        if (selecting) {
+            // Abre un formulario o modal para ingresar detalles del evento
+            const title = prompt('Ingrese el título del evento');
+            if (title) {
+                const newEvent = {
+                    title,
+                    start,
+                    end,
+                };
+                onEventClick(newEvent);
+            }
+            setSelecting(false);
+        } else {
+            setSelecting(true);
+        }
+    };
+
     return (
         <Calendar
             localizer={localizer}
             events={events}
             startAccessor="start"
             endAccessor="end"
-            style={{ height: "500px", backgroundColor: "white" }}
+            selectable={true}
+            onSelectSlot={handleSelectSlot}
+            style={{ height: '500px', backgroundColor: 'white' }}
         />
     );
 };
@@ -23,9 +46,10 @@ CalendarComponent.propTypes = {
             title: PropTypes.string.isRequired,
             start: PropTypes.instanceOf(Date).isRequired,
             end: PropTypes.instanceOf(Date).isRequired,
-            allDay: PropTypes.bool
+            allDay: PropTypes.bool,
         })
-    )
+    ),
+    onEventClick: PropTypes.func.isRequired,
 };
 
 export default CalendarComponent;
